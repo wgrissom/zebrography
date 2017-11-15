d1e01775db89cfb837c17fde5fdbbeca87cd5501
huiwenluo <32940021+huiwenluo@users.noreply.github.com>
2017-11-15 11:09:33 -0600

    Add files via upload

--- fullwavemodel/test_dict.m
+++ fullwavemodel/test_dict.m
@@ -1,9 +1,12 @@
-%example 
+%example
 %hisk(nx*ny*nposition): blurring patterns from captured picetures for slice k.
 %% dictonary matching and looking for corresponding projected waveforms.
+clear proj;
+clear drk;
 for i = 1:size(his,3)
     [num,proj(:,:,i)] = matchfun(his(:,:,i),hisdic,projdic);
-    for j = 1:size(proj,2)
-     drk(:,:,j,i) = recon_eachpoint(proj(:,j,i));
-    end
+    
+end
+for j = 1:size(proj,2)
+    drk(:,:,j,i) = recon_eachpoint(squeeze(proj(:,j,:)));
 end
