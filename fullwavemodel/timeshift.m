76f1919cabb581ad710b3a3547e319485ba5d9b4
huiwenluo <32940021+huiwenluo@users.noreply.github.com>
2018-01-04 11:49:15 -0600

    Add files via upload
    Iterative process for time delay and it's still being modified.

---
+++ fullwavemodel/timeshift.m
@@ -0,0 +1,11 @@
+% projnew(:,:,:,1) = projoritim;
+% projprenew(:,:,:,1) = projpretim;
+% plot(squeeze(projnew(2,:,71,1))','-');
+% hold on;
+% pause(0.005);
+for i=41:45
+    [recon,projnew(:,:,:,i),projprenew,dis(:,:,i),corr(:,:,i),acorr(:,:,:,i)] = iterate_tim(squeeze(projnew(:,:,:,i-1)),squeeze(projprenew));
+    plot(squeeze(projnew(2,:,71,i))','-');
+    hold on;
+    pause(0.005);
+end
