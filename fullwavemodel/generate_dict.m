864b9ff868ed5e5925c903f05ef53204921ec1df
huiwenluo <32940021+huiwenluo@users.noreply.github.com>
2018-01-24 15:23:44 -0600

    Add files via upload

--- fullwavemodel/generate_dict.m
+++ fullwavemodel/generate_dict.m
@@ -1,4 +1,5 @@
-function [hisdic,projdic] = generate_dict(apaz_shifted,z_sv,dX,dT,tTarg)
+function [hisdic,projdic,P] = generate_dict(apaz_shifted,z_sv,dX,dT,tTarg,num)
+%num  slice number of the focus
 
 %% build the dictionary (historgrams -- projected waveforms)
 
@@ -10,13 +11,18 @@ dn0dp = (1.3475-1.3325)/((1100-1)*100000); % dindex/dPascal, from Waxler paper, 
 %%
 P = [];
 % tt = 715:745;
-tt = 1681:1723;
-Pori = apaz_shifted(:,:,tt,:);
+% tt = 1681:1723;
+tt = 635:653;
+ Pori = apaz_shifted(:,:,tt,:);
+% P = Pori;
 for ii = 1:size(Pori,4)
-    P(:,:,:,ii) = permute(interp1(tTarg(tt),permute(squeeze(Pori(:,:,:,ii)),[3 2 1 ]),tTarg(tt(1)):dT/100:tTarg(tt(end)),'cubic',0),[3 2 1]);%   apaz_shifted(:,:,ii) = interp1((t+tShift(ii))',squeeze(apaz_sv(:,:,ii))',tTarg','spline');%,0);
+    P(:,:,:,ii) = permute(interp1(tTarg(tt),permute(squeeze(Pori(:,:,:,ii)),[3 2 1]),tTarg(tt(1)):dT/10:tTarg(tt(end)),'PCHIP',0),[3 2 1]);%   apaz_shifted(:,:,ii) = interp1((t+tShift(ii))',squeeze(apaz_sv(:,:,ii))',tTarg','spline');%,0);
 end
-[x,y] = findpeaks(squeeze(P(71,71,:,4)));
+[x,y] = findpeaks(squeeze(P(round(size(P,1)/2),round(size(P,1)/2),:,num)));
 P = P(:,:,y(1):y(end)-1,:);
+% ppp = permute(P,[3,1,2,4]);
+% ppp = downsample(ppp,9);
+% P = permute(ppp,[2,3,1,4]);
 % P = Pori;
 %% project pressure waveforms
 % calculate the projections using Gmri
@@ -51,8 +57,16 @@ end
 % dx = squeeze(Zd*1/n0*dn0dp*Ix);
 %% generate 2D histogram at each x-z location
 locz = zeros(size(dz));
+flag1 = dz>0;
+flag2 = dz<0;
 for i = 2:size(proj,4)-1
-    locz(:,:,:,i) = dz(:,:,:,i)/(z_sv(i+1)-z_sv(i-1));
+    tmpdz = dz(:,:,:,i);
+    tmplocz = locz(:,:,:,i);
+    tmplocz(flag1(:,:,:,i)) = tmpdz(flag1(:,:,:,i))/(z_sv(i+1)-z_sv(i));
+    tmplocz(flag2(:,:,:,i)) = tmpdz(flag2(:,:,:,i))/(z_sv(i)-z_sv(i-1));
+%     locz(dz>0) = dz(dz>0)/(z_sv(i+1)-z_sv(i));
+%     locz(dz<0) = dz(dz<0)/(z_sv(i)-z_sv(i-1));
+    locz(:,:,:,i) = tmplocz;
 end
 locx = dx/dX;
 
