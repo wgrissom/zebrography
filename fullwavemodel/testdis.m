239ee2d81d39bd53393cc4907f2b6fbd61e8babd
huiwenluo <32940021+huiwenluo@users.noreply.github.com>
2018-02-11 09:03:18 -0600

    Add files via upload

--- fullwavemodel/testdis.m
+++ fullwavemodel/testdis.m
@@ -1,11 +1,13 @@
-apaz = apaz_sv(:,:,2:end);
+% apaz = apaz_sv(:,:,2:end);
 z_sv = cumsum(zvec);
-z_sv = z_sv(abs(z_sv-foc) < 0.25*foc);
+ind = find(abs(z_sv-foc) < 0.5*foc);
+apaz = apaz_sv(:,:,ind);
+z_sv = z_sv(ind);
 num = find(abs(z_sv-foc) == min(abs(z_sv-foc)));
 tTarg = (round((0.5*foc)/c0/dT):round((2*foc)/c0/dT)-1)*dT;
 
 %% shift in z
-apaz = reshape(apaz,[587,1,2881,116]);
+apaz = reshape(apaz,[587,1,2881,size(apaz,3)]);
 zPos = z_sv;
 tShift = zPos/c0; % time shifts (position/(m/s) = s)
 apaz(isnan(apaz)) = 0;
@@ -18,6 +20,7 @@ for ii = 1:size(apaz,4)
     
 end
 % 
+clear apaz_sv
 % %%
 n0 = 1.3325; % base index of refraction
 % m, distance from center of US focus to background
@@ -25,7 +28,7 @@ Zd = 17.061e-2/2;
 dn0dp = (1.3475-1.3325)/((1100-1)*100000); % dindex/dPascal, from Waxler paper, read from Figure 2 at 24.8 deg CSS
 % %%
 P = [];
-tt = 1911:1953;
+tt = 1896:1938;
 Pori = apaz_shifted(:,:,tt,:);
 % P = Pori;
 for ii = 1:size(Pori,4)
@@ -33,7 +36,8 @@ for ii = 1:size(Pori,4)
 end
 [x,y] = findpeaks(squeeze(P(round(size(P,1)/2),1,:,num)));
 P = P(:,:,y(1):y(end),:);
-
+clear apaz_shifted;
+clear Pori
 %% interpolate 2D to 3D profile using interp1
 cen = round([size(P,1),size(P,1)]/2);
 [y1,x1] = meshgrid(1:587,1:587);
@@ -41,6 +45,7 @@ d = (sqrt((x1-cen(1)).^2+(y1-cen(2)).^2))*dX;
 r =(-floor(nX/2):floor(nX/2))*dX;
 d(d>293) = -1;
 Pb = P;
+clear P;
 Pnew = zeros(size(Pb,1),size(Pb,1),size(Pb,3),size(Pb,4));
 for ii = 1:size(Pnew,3)
     for jj = 1:size(Pnew,4)
@@ -71,59 +76,49 @@ for i = 2:size(proj,1)-1
 end
 
 %% for real img
+ds = 3e-3/16/8;
 
-dgrid = 92.6/98*1e-3; %m
-% step = 1;
-z_svnew = [flip(z_sv(num):-dgrid:z_sv(1)),z_sv(num)+dgrid:dgrid:z_sv(end)];
-dxx = interp1((-floor(nX/2)+1:floor(nX/2)-1)*dX,dx(2:end-1,1,:,:),[flip(0:-dgrid:(-floor(nX/2)+1)*dX),dgrid:dgrid:(floor(nX/2)-1)*dX],'pchip');
-dzz = interp1((-floor(nX/2)+1:floor(nX/2)-1)*dX,dz(2:end-1,1,:,:),[flip(0:-dgrid:(-floor(nX/2)+1)*dX),dgrid:dgrid:(floor(nX/2)-1)*dX],'pchip');
-dzz = permute(interp1(z_sv(2:end-1),permute(dzz(:,1,:,2:end-1),[4 3 2 1]),z_svnew(2:end-1),'pchip'),[4 3 2 1]);
-dxx = permute(interp1(z_sv(2:end-1),permute(dxx(:,1,:,2:end-1),[4 3 2 1]),z_svnew(2:end-1),'pchip'),[4 3 2 1]);
-% step = step-1;
+locx = (squeeze(round(dx/(ds))));
+locz = (squeeze(round(dz/(ds))));
+
+locxd = flip(locx,3);   %displacements for locations
+loczd = -flip(locz,3);
+% locxd = 1.2*locx;
+% loczd = 1.2*locz;
+nz = size(loczd,3);
+nt = size(loczd,2);
+xloc = kron(ones(5,1),1:587);
+zloc = kron(ones(5,1),1:nz);
+xloc = xloc(:);
+zloc = zloc(:);
+[zo,xo] = meshgrid(1:nz,1:587);
+[zz,xx] = meshgrid(zloc,xloc);
+% [xo,zo] = meshgrid(1:587,1:nz);
+% [xx,zz] = meshgrid(xloc,zloc);
+for ii = 1:nt
+    locxx(:,ii,:) = interp2(zo,xo,squeeze(locxd(:,ii,:)),zz,xx);
+    loczz(:,ii,:) = interp2(zo,xo,squeeze(loczd(:,ii,:)),zz,xx);
+end
+% locxx = flip(locxx,3);
+[zzz,xxx] = meshgrid(1:length(zloc),1:length(xloc));
+xdisloc = round(repmat(reshape(xxx,size(xxx,1),1,size(xxx,2)),1,nt,1)+locxx);
+zdisloc = round(repmat(reshape(zzz,size(zzz,1),1,size(zzz,2)),1,nt,1)+loczz);
+%caculate coordinates for each pixel at each time point
 
 %%
-img = imread('bkg.png');
-bkg = double(img(2:end,2:end,1));
-bkgnew = double(bkg);
-row = squeeze(bkg(1,:));
-rownum1 = find(row == 0);
-col = squeeze(bkg(:,1));
-colnum1 = find(col == 0);
-row = squeeze(bkg(round(mean(rownum1(1:2))),:));
-rownum2 = find(row == 0);
-col = squeeze(bkg(:,round(mean(colnum1(1:2)))));
-colnum2 = find(col == 0);
 
-xx = (sort([colnum1;colnum2]));
-zz = (sort([rownum1,rownum2]));
-
-
-
-np =  mean(xx(3)-xx(1)); 
-locxd = squeeze(round(dxx/(dgrid/(np/2))));
-loczd = squeeze(round(dzz/(dgrid/(np/2))));
-nphalf = np/2-1;
-% np = np+2;
-% nhalf = floor(np/2);
-% np = np+1;
-nt = size(locxd,2);
-numx = size(locxd,1);
-numz = size(loczd,3);
-% normxz = max(max(abs(locxd(:))),max(abs(loczd(:))));
-% locxd = flip(squeeze(locxd/normxz*nphalf),3);
-% loczd = flip(squeeze(loczd/normxz*nphalf),3);
-cen = floor((3*np)/2);
-for i = 2:numx-10
-    for j = 11:numz-10
-        histmp = 255*ones(3*np,3*np,nt)/nt;
-        v = bkg(xx(i)-nphalf:xx(i)+nphalf,zz(j)-nphalf:zz(j)+nphalf);
-%         display([num2str(i) ' ' num2str(j) ' ' num2str(v) ' ']); 
-        for kk = 1:nt
-            x = round(cen+1+locxd(i,kk,j)); z = round(1+cen+loczd(i,kk,j));
-            histmp(x-nphalf:x+nphalf,z-nphalf:z+nphalf,kk) = v/nt;
-        end
-        histmp = sum(histmp,3);
-        bkgnew(xx(i)-nphalf:xx(i)+nphalf,zz(j)-nphalf:zz(j)+nphalf) = histmp(cen+1-nphalf:cen+1+nphalf,cen+1-nphalf:cen+1+nphalf);
+img = imread('./2018_02_09/IMG_0023.CR2');
+histmp = zeros(size(xdisloc,1),size(xdisloc,3),3);
+partimg = double(img(1+1026:length(xloc)+1026,1+2386+43:length(zloc)+2386+43,:));
+for ii = 1:nt
+    ind = sub2ind([size(xdisloc,1),size(zdisloc,3)],squeeze(xdisloc(:,ii,:)),squeeze(zdisloc(:,ii,:)));
+    for jj = 1:3
+        tmp = partimg(:,:,jj);
+        histmp(:,:,jj)= histmp(:,:,jj) + double(tmp(ind));
     end
 end
-imshow(flip(uint8(bkgnew),2))
+bkg = img;
+bkg(1+1026:length(xloc)+1026,1+2386+43:length(zloc)+2386+43,:) = round(histmp/nt);
+
+figure; imshow(uint8(round(bkg(:,1+2386+43:length(zloc)+2386+43,:))));
+saveas(gcf,'./simuing/simuimg_0023.jpg');   
