0f6902d937ba3e4cd648f7cd112041af962e26a0
huiwenluo <32940021+huiwenluo@users.noreply.github.com>
2018-03-21 16:15:18 -0500

    Add files via upload

--- fullwavemodel/testdis020318.m
+++ fullwavemodel/testdis020318.m
@@ -7,13 +7,13 @@ nX = size(apaz,1);
 %% shift in z
 nT = size(apaz_sv,2);
 apaz = reshape(apaz,[587,1,nT,size(apaz,3)]);
-zPos = z_sv;
-tShift = zPos/c0; % time shifts (position/(m/s) = s)
+% zPos = z_sv;
+% tShift = zPos/c0; % time shifts (position/(m/s) = s)
 apaz(isnan(apaz)) = 0;
 clear apaz_sv
 apaz = squeeze(apaz);
 %%
-apaz(:,size(apaz,2)+1:5000,:) = 0;
+apaz(:,size(apaz,2)+1:5500,:) = 0;
 
 %%
 sig = squeeze(max(max(apaz,[],2),[],1));
@@ -31,20 +31,21 @@ sig = squeeze(max(max(apaz_shifted,[],2),[],1));
 nn = find(sig == max(sig));
 tmp = squeeze(apaz_shifted(294,:,nn));
 tt = find(tmp == max(tmp(:))):find(tmp == max(tmp(:)))+40;
-% tt = 2739:2779;
+% tt = 2739:2779
 P = apaz_shifted(:,tt,:);
+figure;
 plot(squeeze(P(294,:,nn)));
 clear apaz_shifted;
 clear Pori
 %% interpolate 2D to 3D profile using interp1
-P = reshape(P,size(P,1),1,size(P,2),size(P,3));
+P = squeeze(P);
+P = reshape(squeeze(P),size(P,1),1,size(P,2),size(P,3));
 cen = round([size(P,1),size(P,1)]/2);
 [y1,x1] = meshgrid(1:587,1:587);
-d = (sqrt((x1-cen(1)).^2+(y1-cen(2)).^2))*dX;
-r =(-floor(nX/2):floor(nX/2))*dX;
-d(d>293) = -1;
+d = (sqrt((x1-cen(1)).^2+(y1-cen(2)).^2));
+r =(-floor(nX/2):floor(nX/2));
+d(d>293) = -inf;
 Pb = P;
-clear P;
 Pnew = zeros(size(Pb,1),size(Pb,1),size(Pb,3),size(Pb,4));
 for ii = 1:size(Pnew,3) % time dimension
     for jj = 1:size(Pnew,4) % z dimension
@@ -63,30 +64,29 @@ n0 = 1.3325; % base index of refraction
 Zd = 17.061e-2/2;
 dn0dp = (1.3475-1.3325)/((1100-1)*100000); % dindex/dPascal, from Waxler paper, read from Figure 2 at 24.8 deg CSS
 proj = sum(Pnew,2)*dX; % integrate through line of sight
-nt = size(proj,3);
 dz = zeros(size(proj));
 % loop through z-positions and calculated centered differences
-for i = 1+n/2:length(z_svnew)-1-n/2
-    for j = 1:size(proj,1)
-    dz(j,:,:,i) = Zd*1/n0*dn0dp*squeeze(proj(j,:,:,i-n/2:i+n/2))*c/dZ;
+nt = size(proj,3);
+for kk = 1:nt
+    for ii = 1+n/2:length(z_svnew)-1-n/2
+     dz(:,1,kk,ii) = Zd*1/n0*dn0dp*squeeze(proj(:,1,kk,ii-n/2:ii+n/2))*c/dZ;
     end
 end
 dx = zeros(size(proj));
 % loop through x-positions and calculated centered differences
-for i = 1+n/2:size(proj,1)-1-n/2
-    for j = 1:length(z_svnew)
-    dx(i,:,:,j) = Zd*1/n0*dn0dp*c'*squeeze(proj(i-n/2:i+n/2,:,:,j))/dX;
+for kk = 1:nt
+    for ii = 1+n/2:size(proj,1)-1-n/2
+        dx(ii,1,kk,:) = Zd*1/n0*dn0dp*c'*squeeze(proj(ii-n/2:ii+n/2,1,kk,:))/dX;
     end
 end
-
 %% for real img
 ds = 3e-3/16/7;
 
 locx = (squeeze(round(dx/(ds))));
 locz = (squeeze(round(dz/(ds))));
 
-locxd = flip(locx,3);   %displacements for locations
-loczd = -flip(locz,3);
+locxd = locx;%flip(locx,3);   %displacements for locations
+loczd = locz;%-flip(locz,3);
 % locxd = 1.2*locx;
 % loczd = 1.2*locz;
 nz = size(loczd,3);
@@ -114,38 +114,48 @@ zdisloc = round(repmat(reshape(zzz,size(zzz,1),1,size(zzz,2)),1,nt,1)+loczz);
 %caculate coordinates for each pixel at each time point
 
 %%
-img = imread('./2018_02_09/IMG_0021.CR2');
-partimg = double(img(1+153:length(xloc)+153,1+1681:length(zloc)+1681,:));
-nX = size(locxd,1); nZ = size(locxd,3);
-nt = size(locxd,2);
-nn1 = round(dX/ds);
-nn2 = round(dZ/ds); 
-histmp = [];
-img = zeros(size(partimg));
-tmp = zeros(size(xdisloc,1),size(zdisloc,3));
-for kk = 1:nt
-    ind = sub2ind([size(xdisloc,1),size(zdisloc,3)],squeeze(xdisloc(:,kk,:)),squeeze(zdisloc(:,kk,:)));
-    tab = tabulate(ind(:)); % find how many pixels move to the same location 
-    times = tab(:,2);
-    tmp = tmp + reshape((times==0),size(xdisloc,1),size(zdisloc,3));
-    bkg = double(partimg);
-    img = zeros(size(bkg));
-    for ii = 1:nX
-       for jj = 1:nZ
-           block = bkg((1:nn1)+(ii-1)*nn1,(1:nn2)+(jj-1)*nn2,:); % block with the same displacements
-%            bkg((1:nn1)+(ii-1)*nn1,(1:nn2)+(jj-1)*nn2,:) = 0;
-           indx = round((1:nn1)+(ii-1)*nn1+locxd(ii,kk,jj)); % apply displacements and get the shifted locations
-           indz = round((1:nn2)+(jj-1)*nn2+loczd(ii,kk,jj));
-           [tz,tx] = meshgrid(indz,indx);  
-           indind = sub2ind([nX*nn1,nZ*nn2],tx,tz);   
-           ttimes = reshape(tab(indind(:),2),nn1,nn2);   
-           %if ttimes>1 divide it by times to get the sum of pixels which
-           %are shifted to the same location 
-           img(indx,indz,:) = img(indx,indz,:)+block./repmat(ttimes,1,1,3);
-       end
+nnn = 21;%[13,15,17,19,21,23,27,29,37];
+offsetx = 153+nn1;
+offsetz = 1681+nn2;
+for iii = 1:length(nnn)
+     %read photos 
+    img = imread(['./2018_02_09/IMG_00',num2str(nnn(iii)),'.CR2']);
+     %take the samller image field for the simulated pressure
+    partimg = double(img(1+offsetx:length(xloc)+offsetx,1+offsetz:length(zloc)+offsetz,:));
+    % nX the n
+    nX = size(locxd,1); nZ = size(locxd,3);
+    nt = size(locxd,2);
+    nn1 = round(dX/ds);
+    nn2 = round(dZ/ds);
+    histmp = [];
+    img = zeros(size(partimg));
+    tmp = zeros(size(xdisloc,1),size(zdisloc,3));
+    for kk = 1:nt
+        ind = sub2ind([size(xdisloc,1),size(zdisloc,3)],squeeze(xdisloc(:,kk,:)),squeeze(zdisloc(:,kk,:)));
+        tab = tabulate(ind(:)); % find how many pixels move to the same location
+        times = tab(:,2);
+        tmp = tmp + reshape((times==0),size(xdisloc,1),size(zdisloc,3));
+        bkg = double(partimg);
+        img = zeros(size(bkg));
+        for ii = 1:nX
+            for jj = 1:nZ
+                block = bkg((1:nn1)+(ii-1)*nn1,(1:nn2)+(jj-1)*nn2,:); % block with the same displacements
+                %            bkg((1:nn1)+(ii-1)*nn1,(1:nn2)+(jj-1)*nn2,:) = 0;
+                indx = round((1:nn1)+(ii-1)*nn1+locxd(ii,kk,jj)); % apply displacements and get the shifted locations
+                indz = round((1:nn2)+(jj-1)*nn2+loczd(ii,kk,jj));
+                [tz,tx] = meshgrid(indz,indx);
+%                 indind = sub2ind([nX*nn1,nZ*nn2],tx,tz);
+                indind = sub2ind([nX*nn1,nZ*nn2],tx,tz);
+                ttimes = reshape(tab(indind(:),2),nn1,nn2);
+                %if ttimes>1 divide it by times to get the sum of pixels which
+                %are shifted to the same location
+                img(indx,indz,:) = img(indx,indz,:)+block./repmat(ttimes,1,1,3);
+            end
+        end
+        histmp(:,:,:,kk) = img;%bkg;
     end
-    histmp(:,:,:,kk) = img;%bkg;
+    tmp = nt - tmp;
+    figure;
+    imshow(((uint8(sum(histmp,4)./repmat(tmp,1,1,3)))),[]);
+%     saveas(gcf,['./20180315/simuimg_00',num2str(nnn(iii)),'02281.jpg']);
 end
-tmp = nt - tmp;
-imshow(flip(uint8(sum(histmp,4)./tmp),2));
-% saveas(gcf,'./simuimg_00410228.jpg');
